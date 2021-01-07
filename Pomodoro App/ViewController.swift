cab69f50c8eb3ad05ecee86fa3c723284f2c857c
Gamalliel Sharon <you@example.com>
2021-01-07 09:47:33 +0700

    Initial Commit

---
+++ Pomodoro App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro App
+//
+//  Created by Gamalliel Sharon on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
